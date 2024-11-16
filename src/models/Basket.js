9504f89f48e3d0d79859c0f932d5655352e0409b
Vlad0567 <vlad.cheremuchkin@yandex.ru>
2024-11-16 11:42:36 +0300

    delete files

--- src/models/Basket.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const basketSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    productId: { type: String, required: true },
-    quantity: { type: Number, default: 1 }
-});
-
-module.exports = mongoose.model('Basket', basketSchema);
--- src/models/Favorite.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const favoriteSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true }
-});
-
-module.exports = mongoose.model('Favorite', favoriteSchema);
