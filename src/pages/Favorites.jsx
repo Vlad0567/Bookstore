b94287e2b70f36b3a123bb89fe4507587938f3c2
Vlad0567 <vlad.cheremuchkin@yandex.ru>
2024-11-16 11:59:42 +0300

    update files

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,38 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import ProductGrid from '../components/widgets/ProductGrid';
-import { useNavigate } from 'react-router-dom';
+import booksData from '../assets/books.json'; // Импортируем данные о книгах
 
 const Favorites = () => {
     const [favorites, setFavorites] = useState([]);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const navigate = useNavigate();
 
     useEffect(() => {
-        const authStatus = localStorage.getItem('isAuthenticated');
-        setIsAuthenticated(authStatus === 'true');
-
-        if (authStatus === 'true') {
-            fetchFavorites();
-        }
+        // Извлечение избранных id из localStorage
+        const favoriteIds = JSON.parse(localStorage.getItem('favorites')) || [];
+        
+        // Получаем информацию о книгах по их id
+        const favoriteBooks = booksData.filter(book => favoriteIds.includes(book._id));
+        
+        setFavorites(favoriteBooks);
     }, []);
-
-    const fetchFavorites = async () => {
-        try {
-            const userId = localStorage.getItem('userId');
-            const response = await fetch(`http://localhost:5000/api/favorites/${userId}`);
-            if (!response.ok) {
-                throw new Error(`Ошибка сервера: ${response.status}`);
-            }
-            const data = await response.json();
-            setFavorites(data.favorites || []);
-        } catch (error) {
-            console.error('Ошибка при загрузке избранного:', error);
-        }
-    };
-
-    if (!isAuthenticated) {
-        return <p>Чтобы увидеть свои избранные товары, войдите в свой аккаунт.</p>;
-    }
 
     return (
         <div className="favorites-page">
